104685df09ce5e37b1d46c24271bde4ab9e49e40
KhanhHoang <Khanhsingapore@gmail.com>
2020-04-17 09:52:18 -0500

    prompt for role input

--- app.js
+++ app.js
@@ -13,27 +13,45 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+const NewEmployees = [];
+GetEmployees();
 const GeneralQuestion = [
     {
         type: "input",
-        message: "Enter the ${role}'s name:",
+        message: "Enter the employee's name:",
         name: "name"
     },
     {
         type: "number",
-        message: "Enter ${role}'s ID:",
+        message: "Enter employee's ID:",
         name: "id"
     },
     {
         type: "input",
-        message: "Enter the ${role}'s email:",
+        message: "Enter the employee's email:",
         name: "email"
     }
 ]
-const NewEployee = [];
-function NewEmployee() {
-    const role = "";
-    if (role === "manage") {
+async function promptrole() {
+    await GetEmployees();
+    return inquirer
+        .prompt({
+            type: "list",
+            message: "Which role would you like to add?",
+            name: "role",
+            choices: [
+                "Manager",
+                "Engineer",
+                "Intern",
+
+            ]
+        })
+}
+
+function GetEmployees() {
+    const role = promptrole();
+    if (role === "Manager") {
         return inquirer
             .prompt([
                 ...GeneralQuestion,
@@ -58,11 +76,11 @@ function NewEmployee() {
                     name: "officeNumber"
                 }
             ]).then(function ({ name, id, email, github }) {
-                NewEmployee.push(new Manager(name, id, email, github));;
-            });
+                NewEmployees.push(new Manager(name, id, email, github));
+                return GetEmployees();
+            }) 
 
-    }
-    else if (role === "engineer") {
+    } else if (role === "Mangineer") {
         return inquirer
             .prompt([
                 ...GeneralQuestion,
@@ -87,11 +105,10 @@ function NewEmployee() {
                     name: "github"
                 }
             ]).then(function ({ name, id, email, github }) {
-                NewEmployee.push(new Engineer(name, id, email, github));
+                NewEmployees.push(new Engineer(name, id, email, github));
+                return GetEmployees();
             });
-    }
-
-    else (role === "intern") {
+    } else if (role === "Intern") {
         return inquirer
             .prompt([
                 ...GeneralQuestion,
@@ -116,22 +133,40 @@ function NewEmployee() {
                     name: "school"
                 }
             ]).then(function ({ name, id, email, school }) {
-                NewEployee.push(new Intern(name, id, email, school));
-            });
+                NewEmployees.push(new Intern(name, id, email, school));
+                return GetEmployees();
+            }) 
+    } else {
+        return NewEmployees;
     }
 };
+function promptrole() {
+    return inquirer
+        .prompt({
+            type: "list",
+            message: "Which role would you like to add?",
+            name: "role",
+            choices: [
+                "Manager",
+                "Engineer",
+                "Intern",
+
+            ]
+        })
+}
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-const NewTeam = render(newEmployees)
+const Team = render(NewEmployees);
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-fs.writeFile(outputPath, NewTeam, function (err) {
+fs.writeFile(outputPath, Team, function (err) {
     if (err) {
         console.log(err);
     }
